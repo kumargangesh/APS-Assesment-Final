62de757b92002ec2f91530aa107541faedcce55f
GANGESH KUMAR <kumargangesh090@gmail.com>
2026-08-19 15:26:11 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,11 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+// app.use(cors());
+app.use(cors({
+  origin:'*'
+}));
+
 app.use(express.json());
 
 app.use('/api/auth', require('./routes/authRoutes'));
